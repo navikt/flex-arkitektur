68cee045324872ee91cec2c9c1ce6bafda527247
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-04 19:08:09 +0100

    filter på cluster

--- src/app/api/bq/route.ts
+++ src/app/api/bq/route.ts
@@ -2,11 +2,11 @@ export const dynamic = 'force-dynamic'
 import { NextResponse } from 'next/server'
 import { BigQuery } from '@google-cloud/bigquery'
 
-export async function GET(): Promise<NextResponse<App[]>> {
+export async function GET(req: Request): Promise<NextResponse<App[]>> {
     const bigquery = new BigQuery()
-
+    const params = new URL(req.url).searchParams
+    const cluster = params.get('cluster') || 'prod'
     const query = `SELECT name,
-                          collection_time,
                           cluster,
                           namespace,
                           team,
@@ -16,8 +16,8 @@ export async function GET(): Promise<NextResponse<App[]>> {
                           outbound_hosts,
                           read_topics,
                           write_topics
-                   FROM \`aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`
-                   WHERE dato = (SELECT MAX(dato) FROM \`aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`)`
+                   FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`
+                   WHERE dato = (SELECT MAX(dato) FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`)`
 
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
@@ -32,13 +32,26 @@ export async function GET(): Promise<NextResponse<App[]>> {
     // Wait for the query to finish
     const [rows] = await job.getQueryResults()
     // Print the results
+    const apps: App[] = rows.map((row) => {
+        return {
+            name: row.name,
+            cluster: row.cluster,
+            namespace: row.namespace,
+            team: row.team,
+            ingresses: row.ingresses,
+            inbound_apps: row.inbound_apps,
+            outbound_apps: row.outbound_apps,
+            outbound_hosts: row.outbound_hosts,
+            read_topics: row.read_topics,
+            write_topics: row.write_topics,
+        }
+    })
 
-    return NextResponse.json(rows)
+    return NextResponse.json(apps.filter((app) => app.cluster.includes(cluster)))
 }
 
 interface App {
     name: string
-    collection_time: string
     cluster: string
     namespace: string
     team: string
