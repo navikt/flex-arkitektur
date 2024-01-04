56886d2253dbc08767de3d02b76dd71929ed4977
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-04 17:31:31 +0100

    hent data

--- src/app/api/bq/route.ts
+++ src/app/api/bq/route.ts
@@ -1,38 +1,51 @@
 export const dynamic = 'force-dynamic'
 import { NextResponse } from 'next/server'
 import { BigQuery } from '@google-cloud/bigquery'
-import { logger } from '@navikt/next-logger'
 
-export async function GET(req: Request): Promise<NextResponse<Record<string, string>>> {
-    try {
-        const text = await req.text()
-        logger.info('bq api request' + text)
+export async function GET(): Promise<NextResponse<App[]>> {
+    const bigquery = new BigQuery()
 
-        const bigquery = new BigQuery()
+    const query = `SELECT name,
+                          collection_time,
+                          cluster,
+                          namespace,
+                          team,
+                          ingresses,
+                          inbound_apps,
+                          outbound_apps,
+                          outbound_hosts,
+                          read_topics,
+                          write_topics
+                   FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`
+                   WHERE dato = (SELECT MAX(dato) FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`)`
 
-        const query = `SELECT name, cluster
-                       FROM \`aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`
-                       where team = 'flex'
-                       LIMIT 20`
-
-        // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-        const options = {
-            query: query,
-            // Location must match that of the dataset(s) referenced in the query.
-            location: 'europe-north1',
-        }
-
-        // Run the query as a job
-        const [job] = await bigquery.createQueryJob(options)
-
-        // Wait for the query to finish
-        const [rows] = await job.getQueryResults()
-        logger.info('bq api request' + JSON.stringify(rows))
-        // Print the results
-
-        return NextResponse.json({ res: JSON.stringify(rows), ok: 'true' })
-    } catch (error) {
-        logger.error('bq api request' + error)
-        return NextResponse.json({ res: JSON.stringify(error), error: 'true' })
+    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+    const options = {
+        query: query,
+        // Location must match that of the dataset(s) referenced in the query.
+        location: 'europe-north1',
     }
+
+    // Run the query as a job
+    const [job] = await bigquery.createQueryJob(options)
+
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults()
+    // Print the results
+
+    return NextResponse.json(rows)
+}
+
+interface App {
+    name: string
+    collection_time: string
+    cluster: string
+    namespace: string
+    team: string
+    ingresses: string
+    inbound_apps: string
+    outbound_apps: string
+    outbound_hosts: string
+    read_topics: string
+    write_topics: string
 }
