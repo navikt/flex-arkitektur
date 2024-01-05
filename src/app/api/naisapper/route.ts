0792993da6814ade7974ed651f35039d16e77a31
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-05 07:29:33 +0100

    browser cache

--- src/app/api/naisapper/route.ts
+++ src/app/api/naisapper/route.ts
@@ -49,7 +49,10 @@ export async function GET(req: Request): Promise<NextResponse<App[]>> {
         })
         lastFetchTime = currentTime
     }
-    return NextResponse.json(cachedData.filter((app) => app.cluster.includes(cluster)))
+    const nextResponse = NextResponse.json(cachedData.filter((app) => app.cluster.includes(cluster)))
+    nextResponse.headers.set('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=3600')
+
+    return nextResponse
 }
 
 interface App {
