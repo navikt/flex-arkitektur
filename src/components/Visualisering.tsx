1ccce30262bf50dd018c73890ddbede5690d7d53
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-05 08:54:58 +0100

    Namespace og miljø

--- src/components/Visualisering.tsx
+++ src/components/Visualisering.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { parseAsArrayOf, parseAsString, useQueryState } from 'next-usequerystate'
-import { Alert } from '@navikt/ds-react'
+import { Alert, Select, UNSAFE_Combobox } from '@navikt/ds-react'
 
 import { NaisApp } from '@/types'
 import { fetchJsonMedRequestId } from '@/utlis/fetch'
 
 export const Visualisering = (): ReactElement => {
-    const [env] = useQueryState('env', parseAsString.withDefault('prod'))
+    const [env, setEnv] = useQueryState('env', parseAsString.withDefault('prod'))
 
-    const [namespace] = useQueryState('namespace', parseAsArrayOf(parseAsString).withDefault(['flex']))
+    const [namespaces, setNamespaces] = useQueryState('namespace', parseAsArrayOf(parseAsString).withDefault(['flex']))
     const { data, error, isFetching } = useQuery<NaisApp[], Error>({
         queryKey: [`nais-apper`, env],
         queryFn: async () => {
@@ -26,10 +26,37 @@ export const Visualisering = (): ReactElement => {
         return <Alert variant="error">Kunne ikke hente data fra api</Alert>
     }
 
+    // unike namespaces fra data
+    const alleNamespaces = Array.from(new Set(data.map((app) => app.namespace)))
+    const onToggleSelected = (option: string, isSelected: boolean): void => {
+        if (isSelected) {
+            setNamespaces([...namespaces, option])
+        } else {
+            setNamespaces(namespaces.filter((o) => o !== option))
+        }
+    }
     return (
         <>
+            <UNSAFE_Combobox
+                label="Velg namespace"
+                options={alleNamespaces}
+                isMultiSelect
+                selectedOptions={namespaces}
+                onToggleSelected={onToggleSelected}
+            />
+            <Select
+                label="Miljø"
+                description="Velg det landet du tilbringer mest tid."
+                value={env}
+                onChange={(e) => {
+                    setEnv(e.target.value)
+                }}
+            >
+                <option value="prod">Produksjon</option>
+                <option value="dev">Development</option>
+            </Select>
             {data
-                .filter((app) => namespace.includes(app.namespace))
+                .filter((app) => namespaces.includes(app.namespace))
                 .map((app) => (
                     <div key={app.name}>
                         <h2>{app.name}</h2>
