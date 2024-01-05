8c479e914b41ce00a597f5b36b74a24ec00b13ba
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-05 10:42:21 +0100

    Bedre graf

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -8,7 +8,18 @@ export function Graph({ apper, namespaces }: { apper: NaisApp[]; namespaces: str
 
     const filtreteApper = apper.filter((app) => namespaces.includes(app.namespace))
 
-    const nodes = filtreteApper.map((app) => ({ id: name(app), label: app.name }))
+    const nodes = filtreteApper.map((app) => {
+        return {
+            id: name(app),
+            label: `${namespaceToEmoji(app.namespace)} ${app.name}`,
+            shape: 'box',
+            group: app.namespace,
+            font: {
+                face: 'monospace',
+                align: 'left',
+            },
+        }
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const edges = [] as { from: string; to: string }[] //TODO memo
 
@@ -19,7 +30,37 @@ export function Graph({ apper, namespaces }: { apper: NaisApp[]; namespaces: str
     })
 
     useEffect(() => {
-        container.current && new Network(container.current, { nodes, edges }, {})
+        container.current &&
+            new Network(
+                container.current,
+                { nodes, edges },
+                {
+                    groups: {
+                        noAuthConnection: {
+                            color: { color: '#ff5a5a', highlight: '#ff5a5a', hover: '#ff5a5a' },
+                        },
+                        teamsykmelding: {
+                            font: {
+                                face: 'monospace',
+                                align: 'left',
+                                color: '#ffffff',
+                            },
+                            color: { background: 'green', border: 'darkgreen' },
+                        },
+                        flex: {
+                            font: {
+                                face: 'monospace',
+                                align: 'left',
+                                color: '#ffffff',
+                            },
+                            color: { background: 'red', border: 'darkgreen' },
+                        },
+                    },
+                    physics: {
+                        solver: 'forceAtlas2Based',
+                    },
+                },
+            )
     }, [container, nodes, edges])
 
     return <div ref={container} style={{ height: 'calc(100vh - var(--a-spacing-32))' }} />
@@ -27,4 +68,23 @@ export function Graph({ apper, namespaces }: { apper: NaisApp[]; namespaces: str
 
 function name(app: NaisApp): string {
     return `${app.cluster}.${app.namespace}.${app.name}`
+}
+
+function namespaceToEmoji(namespace: string): string {
+    switch (namespace) {
+        case 'teamsykmelding':
+            return '💉'
+        case 'flex':
+            return '💪'
+        case 'team-esyfo':
+            return '🫂'
+        case 'risk':
+            return '☣️'
+        case 'teamsykefravr':
+            return '🏥'
+        case 'personbruker':
+            return '🧑🏽'
+        default:
+            return ''
+    }
 }
