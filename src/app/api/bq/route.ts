3ee0b223bae22c24d5c477ffa596410afcb91b0f
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-05 07:02:35 +0100

    Fix spørring

--- src/app/api/bq/route.ts
+++ src/app/api/bq/route.ts
@@ -6,38 +6,31 @@ export async function GET(req: Request): Promise<NextResponse<App[]>> {
     const bigquery = new BigQuery()
     const params = new URL(req.url).searchParams
     const cluster = params.get('cluster') || 'prod'
+    const bqTabell = '`aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3`'
     const query = `SELECT name,
                           cluster,
                           namespace,
-                          team,
                           ingresses,
                           inbound_apps,
                           outbound_apps,
                           outbound_hosts,
                           read_topics,
                           write_topics
-                   FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`
-                   WHERE dato = (SELECT MAX(dato) FROM \` aura-prod-d7e3.dataproduct_apps.dataproduct_apps_unique_v3\`)`
+                   FROM ${bqTabell}
+                   WHERE dato = (SELECT MAX(dato) FROM ${bqTabell})`
 
-    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-    const options = {
+    const [job] = await bigquery.createQueryJob({
         query: query,
-        // Location must match that of the dataset(s) referenced in the query.
         location: 'europe-north1',
-    }
+    })
 
-    // Run the query as a job
-    const [job] = await bigquery.createQueryJob(options)
-
-    // Wait for the query to finish
     const [rows] = await job.getQueryResults()
-    // Print the results
+
     const apps: App[] = rows.map((row) => {
         return {
             name: row.name,
             cluster: row.cluster,
             namespace: row.namespace,
-            team: row.team,
             ingresses: row.ingresses,
             inbound_apps: row.inbound_apps,
             outbound_apps: row.outbound_apps,
@@ -54,7 +47,6 @@ interface App {
     name: string
     cluster: string
     namespace: string
-    team: string
     ingresses: string
     inbound_apps: string
     outbound_apps: string
