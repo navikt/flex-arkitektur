09fe82c9a063955130c1912fce1a1f6e8fc43495
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-11 08:10:50 +0100

    Revert "Tillatt lokalt kall"
    This reverts commit 8bb8f628c8b667a91452f0bf2058e927700826a0.

--- src/app/api/v1/naisapper/route.ts
+++ src/app/api/v1/naisapper/route.ts
@@ -3,15 +3,17 @@ import { NextResponse } from 'next/server'
 
 import { hentNaisApper } from '@/bigquery/naisAppsFetching'
 import { NaisApp } from '@/types'
+import { verifyUserLoggedIn } from '@/auth/authentication'
 
 let cachedData: NaisApp[] | undefined = undefined
 let lastFetchTime = 0
 
 export async function GET(req: Request): Promise<NextResponse<NaisApp[]>> {
+    await verifyUserLoggedIn()
     const currentTime = Date.now()
     const params = new URL(req.url).searchParams
     const cacheTimer = 6
-    const cluster = params.get('env')
+    const cluster = params.get('env') || 'prod'
 
     if (!cachedData || currentTime - lastFetchTime > 3600000 * cacheTimer) {
         logger.info('Henter naisapper fra bigquery')
@@ -20,9 +22,7 @@ export async function GET(req: Request): Promise<NextResponse<NaisApp[]>> {
     } else {
         logger.info('Henter naisapper fra cache')
     }
-    const nextResponse = NextResponse.json(
-        cachedData.filter((app) => (!cluster ? true : app.cluster.includes(cluster))),
-    )
+    const nextResponse = NextResponse.json(cachedData.filter((app) => app.cluster.includes(cluster)))
     nextResponse.headers.set('Cache-Control', 'public, s-maxage=300, stale-while-revalidate=300')
 
     return nextResponse
