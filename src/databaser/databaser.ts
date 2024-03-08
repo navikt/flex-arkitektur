27319147aa41bfa7354c4626f5819df42fb7ae6a
Karl O <k@rl.run>
2024-03-08 11:17:56 +0000

    Legg til teamsykmelding sine databaser (#171)

--- src/databaser/databaser.ts
+++ src/databaser/databaser.ts
@@ -1,4 +1,4 @@
-export const databaser = [
+const flexDatabaser = [
     {
         appName: 'ditt-sykefravaer-backend',
         namespace: 'flex',
@@ -75,3 +75,98 @@ export const databaser = [
         databases: ['sykepengesoknad-sak-status-metrikk-db'],
     },
 ]
+
+const teamsykmeldingDatabaser = [
+    {
+        appName: 'dinesykmeldte-backend',
+        namespace: 'teamsykmelding',
+        databases: ['dinesykmeldte-backend'],
+    },
+    {
+        appName: 'helsesjekk-bot',
+        namespace: 'teamsykmelding',
+        databases: ['helsesjekk-bot'],
+    },
+    {
+        appName: 'narmesteleder',
+        namespace: 'teamsykmelding',
+        databases: ['narmesteleder'],
+    },
+    {
+        appName: 'narmesteleder-arbeidsforhold',
+        namespace: 'teamsykmelding',
+        databases: ['narmesteleder-arbeidsforhold'],
+    },
+    {
+        appName: 'narmesteleder-varsel',
+        namespace: 'teamsykmelding',
+        databases: ['narmesteleder-varsel'],
+    },
+    {
+        appName: 'pale-2',
+        namespace: 'teamsykmelding',
+        databases: ['pale-2'],
+    },
+    {
+        appName: 'pale-2-register',
+        namespace: 'teamsykmelding',
+        databases: ['pale-2-register'],
+    },
+    {
+        appName: 'smregistrering-backend',
+        namespace: 'teamsykmelding',
+        databases: ['smregistrering'],
+    },
+    {
+        appName: 'sparenaproxy',
+        namespace: 'teamsykmelding',
+        databases: ['sparenaproxy'],
+    },
+    {
+        appName: 'syfonlaltinn',
+        namespace: 'teamsykmelding',
+        databases: ['syfonlaltinn'],
+    },
+    {
+        appName: 'syfosmaltinn',
+        namespace: 'teamsykmelding',
+        databases: ['syfosmaltinn'],
+    },
+    {
+        appName: 'syfosmmanuell-backend',
+        namespace: 'teamsykmelding',
+        databases: ['syfosmmanuell-backend'],
+    },
+    {
+        appName: 'syfosmmottak',
+        namespace: 'teamsykmelding',
+        databases: ['syfosmmottak'],
+    },
+    {
+        appName: 'syfosmregister',
+        namespace: 'teamsykmelding',
+        databases: ['smregister'],
+    },
+    {
+        appName: 'syfosmvarsel',
+        namespace: 'teamsykmelding',
+        databases: ['smvarsel'],
+    },
+    {
+        appName: 'syk-dig-backend',
+        namespace: 'teamsykmelding',
+        databases: ['syk-dig-backend'],
+    },
+    {
+        appName: 'sykmeldinger-arbeidsgiver',
+        namespace: 'teamsykmelding',
+        databases: ['sykmeldinger-arbeidsgiver'],
+    },
+    {
+        appName: 'sykmeldinger-backend',
+        namespace: 'teamsykmelding',
+        databases: ['sykmeldinger'],
+    },
+]
+
+export const databaser = [...flexDatabaser, ...teamsykmeldingDatabaser]
